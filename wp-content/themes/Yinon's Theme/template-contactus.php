c451f633afc980dc41526aaee2aed8a52d7e026b
Yinon bar <yinonbar1988@gmail.com>
2023-02-19 16:06:25 +0200

    clean slate

---
+++ wp-content/themes/Yinon's Theme/template-contactus.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/* Template name: contact us */
+
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+  <h1><?php the_title(); ?></h1>
+
+  <div class="row">
+    <div class="col-lg-6">
+      <p>this is where the contact form goes</p>
+    </div>
+    <div class="col-lg-6">
+      <?php get_template_part('includes/section', 'content') ?>
+
+    </div>
+  </div>
+
+
+
+</div>
+
+
+<?php get_footer(); ?>
